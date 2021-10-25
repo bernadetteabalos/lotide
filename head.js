7b5ef80e30bc205826b7321fa92682253bf2ff84
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-10-25 18:36:45 +0000

    Implemented a function to extract the first element of an array.

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+console.log(head([]));
+console.log(head([5,6,7]));
