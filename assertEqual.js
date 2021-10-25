8570928e918425a508939aa79e1f9b785a8f21f2
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-10-25 18:17:25 +0000

    Created an assertion function.

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return "✅✅✅ Assertion Passed: " + actual + ' === ' + expected;
+  } else {
+    return "🛑🛑🛑 Assertion Failed: " + actual + ' !== ' + expected;
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1,1));
+console.log(assertEqual("Pie", "Pie"));
+console.log(assertEqual(2, 3));
