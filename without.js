6f483160a5b3c69ec479337592da155603df3c6d
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-10-26 21:06:00 +0000

    Added function to filter out unwaned items in an array.

---
+++ without.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+      return;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+};
+
+const without = function(source, itemsToRemove) {
+  let newArray = source.slice(); 
+  // let newArray2 = [...source]
+  for (let i = 0; i < newArray.length; i++) {
+    for (let x = 0; x < itemsToRemove.length; x++) {
+      if (newArray[i] === itemsToRemove[x]) {
+        newArray.splice(i, 1);
+      }
+    }
+  }
+  return newArray;
+};
+
+let original = [3,4,5]
+console.log(without(original, [4]));
+assertArraysEqual([3,4], without(original, [5,2]));
+
+console.log(original);
+
+assertArraysEqual(["1","2"], without(["1", "2", "3"], [1, 2, "3"]))
