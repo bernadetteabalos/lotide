f34b88b6ed83893e36611d755e6813ea0e3955f2
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-10-27 00:50:02 +0000

    Created function to extract middle of an array.

---
+++ middle.js
@@ -0,0 +1,48 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+      return;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+};
+
+
+const middle = function(array) {
+  let output = [];
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    let x = array.length / 2;
+    output.push(array[x - 1]);
+    output.push(array[x]);
+    return output;
+  } else if (array.length % 2 !== 0) {
+    let y = array.length / 2;
+    output.push(array[Math.floor(y)]);
+    return output;
+  }
+};
+// array.length <= 2
+// return empty array
+
+// array.length % 2 === 0
+// x = array.length / 2, x + 1
+
+// array.length % 2 !== 0
+// array.length / 2 -- math.floor()
+
+// TEST CODE
+
+assertArraysEqual([], middle([1, 2]));
+assertArraysEqual(['goodbye'], middle(['hellow','goodbye','yellow']));
