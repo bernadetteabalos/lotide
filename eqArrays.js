b025c614bd44124680875bfea8d653b239fcf409
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-11-01 17:35:16 +0000

    Refactored the eqArrays function and added a eqArrays test file.

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
@@ -18,8 +11,5 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return true;
 };
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false));
+
+module.exports = eqArrays;
