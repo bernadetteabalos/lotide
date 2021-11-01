c1c93e6da2e6246b67523f9e3c53bfa71daab6fa
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-11-01 22:28:44 +0000

    Implemented chai and mocha into middle and tail test functions.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,20 @@
+const assert = require('chai').assert;
+const middle = require ('../middle');
+
+describe('#middle', () => {
+  it("returns empty array for arrays with two or less elements", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+
+  it("returns middle element of array ['goodbye']", () => {
+    assert.deepEqual(middle(['hellow','goodbye','yellow']), ['goodbye']);
+  });
+});
+
+/*
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require ('../middle')
 
 assertArraysEqual([], middle([1,2]));
 assertArraysEqual(middle(['hellow','goodbye','yellow']), ['goodbye']);
+*/
